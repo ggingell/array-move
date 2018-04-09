38f915542f90d8947bdb2afb88390f23dffa5bec
ggingell <1558535+ggingell@users.noreply.github.com>
2018-04-08 22:15:34 -0700

    Array move with unit tests

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+function move(collection, fromIndex, toIndex) {
+    var result = [];
+    var shift = Math.sign(toIndex - fromIndex);
+    var start = Math.min(fromIndex, toIndex);
+    var end = Math.max(fromIndex, toIndex);
+
+    for (var index = 0; index < collection.length; index++) {
+        var offset = index >= start && index <= end ? shift : 0;
+
+        value = collection[index + offset];
+        result.push(value);
+    }
+
+    result[toIndex] = collection[fromIndex];
+
+    return result;
+}
+
+module.exports = move;
---
+++ src/index.test.js
@@ -0,0 +1,47 @@
+
+var move = require('./');
+
+describe('move', function () {
+    var CASE_A;
+    var CASE_B;
+    var CASE_C;
+
+    beforeEach(function () {
+        CASE_A = ['a', 'b'];
+        CASE_B = ['a','b','c'];
+        CASE_C = ['a','b','c','d','e','f'];
+    });
+
+    it('moves', function () {
+
+        expect(move(CASE_A, 0, 0)).toEqual(['a','b']);
+        expect(move(CASE_A, 1, 1)).toEqual(['a','b']);
+
+        expect(move(CASE_A, 0, 1)).toEqual(['b','a']);
+        expect(move(CASE_A, 1, 0)).toEqual(['b','a']);
+
+        expect(move(CASE_B, 0, 1)).toEqual(['b','a','c']);
+        expect(move(CASE_B, 1, 0)).toEqual(['b','a','c']);
+        expect(move(CASE_B, 2, 0)).toEqual(['c','a','b']);
+        expect(move(CASE_B, 0, 2)).toEqual(['b','c','a']);
+        expect(move(CASE_B, 1, 2)).toEqual(['a','c','b']);
+        expect(move(CASE_B, 2, 1)).toEqual(['a','c','b']);
+
+        expect(move(CASE_C, 0, 1)).toEqual(['b','a','c','d','e','f']);
+        expect(move(CASE_C, 1, 4)).toEqual(['a','c','d','e','b','f']);
+        expect(move(CASE_C, 4, 2)).toEqual(['a','b','e','c','d','f']);
+        expect(move(CASE_C, 0, 5)).toEqual(['b','c','d','e','f','a']);
+        expect(move(CASE_C, 5, 0)).toEqual(['f','a','b','c','d','e']);
+    });
+
+    function _arrayMove(x, from, to) {
+        x.splice((to < 0 ? x.length + to : to), 0, x.splice(from, 1)[0]);
+    }
+
+    function arrayMove(x, from, to) {
+        x = x.slice();
+        _arrayMove(x, from, to);
+        return x;
+    };
+
+});
