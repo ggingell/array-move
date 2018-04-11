0d61b2c632b80cf1ca33cdf52ffac97cebd7b0f2
ggingell <1558535+ggingell@users.noreply.github.com>
2018-04-10 20:20:39 -0700

    Remove need for the Math.* statements by using ternary

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-'use strict';
+"use strict";
 
 function move(collection, fromIndex, toIndex) {
-    var result = [];
-    var shift = Math.sign(toIndex - fromIndex);
-    var start = Math.min(fromIndex, toIndex);
-    var end = Math.max(fromIndex, toIndex);
+  var result = [];
+  var shift = toIndex - fromIndex > 0 ? 1 : -1;
+  var start = toIndex < fromIndex ? toIndex : fromIndex;
+  var end = start < toIndex ? toIndex : fromIndex;
 
-    for (var index = 0; index < collection.length; index++) {
-        var offset = index >= start && index <= end ? shift : 0;
+  for (var index = 0; index < collection.length; index++) {
+    var offset = index >= start && index <= end ? shift : 0;
+    result.push(collection[index + offset]);
+  }
 
-        value = collection[index + offset];
-        result.push(value);
-    }
+  result[toIndex] = collection[fromIndex];
 
-    result[toIndex] = collection[fromIndex];
-
-    return result;
+  return result;
 }
 
 module.exports = move;
